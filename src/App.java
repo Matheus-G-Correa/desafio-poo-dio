0491829d040da3bef156ddba13e9f1604f5364b2
Matheus-G-Correa <teteus.kratos@hotmail.com>
2023-04-17 22:20:31 -0300

    segundo commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,30 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Saudações!");
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso  Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        
+        curso2.setTitulo("Curso  Js");
+        curso2.setDescricao("Descrição curso Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
